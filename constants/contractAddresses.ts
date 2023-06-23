fa74824e79d04bc7e77f4d32fd2389c294dd9a22
Weston Nelson <dogooddowell@protonmail.com>
2023-06-23 02:47:02 +0000

    Add WalletConnect2.0

---
+++ constants/contractAddresses.ts
@@ -0,0 +1,10 @@
+export const earthlingContractAddress =
+  "0x8778B7FD7e2480C6F9Ad1075Bd848B7Ce1b9d90C";
+export const nfteTokenContractAddress =
+  "0xB261104A83887aE92392Fb5CE5899fCFe5481456";
+export const roboroverContractAddress =
+  "0xe148cb1b6b2B040FEA525856A9E2F6E25115189A";
+export const nfw3cContractAddress =
+  "0xB0CbF85907896918aaf9Aa27773637c9F8b5b0cc";  
+export const stakingContractAddress =
+  "0xb37cd5fF087116B6Af620C69DeC2a03Ca5e5CaDe";
