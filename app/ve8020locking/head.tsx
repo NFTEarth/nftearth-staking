5595a72e5b851aeaa7cc7475fb1a936cfb4ab097
Weston Nelson <dogooddowell@protonmail.com>
2023-06-25 04:18:27 -0500

    Create head.tsx
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

---
+++ app/ve8020locking/head.tsx
@@ -0,0 +1,33 @@
+export default function Head() {
+  return (
+    <>
+      <title>NFTEarth Staking</title>
+      <meta name="viewport" content="width=device-width, initial-scale=0.5" />
+
+      <meta
+        name="description"
+        content="NFTEarth Staking user interface with batch actions and other improvements."
+      />
+      <meta
+        property="og:title"
+        content="NFTEarth Staking user interface with batch actions and other improvements."
+      />
+      <meta property="og:site_name" content="NFTEarth Staking" />
+      <meta property="og:url" content="https://staking.nftearth.exchange" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://staking.nftearth.exchange/opengraph.png" />
+      <meta name="twitter:site" content="@NFTEarth_L2" />
+      <meta name="twitter:title" content="NFTEarth" />
+      <meta
+        name="twitter:description"
+        content="NFTEarth Staking user interface with batch actions and other improvements."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:image:src"
+        content="https://staking.nftearth.exchange/opengraph.png"
+      />
+      <link rel="icon" href="./teeth.png" />
+    </>
+  );
+}
