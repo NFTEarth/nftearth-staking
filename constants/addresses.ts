0bbe2ce59b8a58ee8858b461dddaeb18af5e7fb4
Weston Nelson <dogooddowell@protonmail.com>
2023-06-26 20:38:47 +0000

    Update

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -1,4 +1,4 @@
 export const stakingContractAddresses =
   "0x00e3aA03e47c32397a94509E50B0558988C0D04E";
 export const nfteContractAddresses =
-  "0xc96f4F893286137aC17e07Ae7F217fFca5db3AB6";
+  "0xB261104A83887aE92392Fb5CE5899fCFe5481456";
