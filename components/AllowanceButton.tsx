91c1250f862e3d8298115301f25c9efe571be23f
Weston Nelson <dogooddowell@protonmail.com>
2023-06-27 23:23:42 +0000

    Add Stake Function Button

--- components/AllowanceButton.tsx
+++ components/AllowanceButton.tsx
@@ -9,9 +9,7 @@ import {
   usePrepareContractWrite,
   useWaitForTransaction,
 } from "wagmi";
-
 import ABI from "../abis/nfteToken";
-
 import {
   CHAIN_ID,
   nfteContractAddresses,
@@ -30,7 +28,7 @@ export default function AllowanceButton() {
     args: [address as `0x${string}`, stakingContractAddresses[chain?.id || CHAIN_ID]],
   });
 
-  const { config } = usePrepareContractWrite({
+  const { config: approveConfig } = usePrepareContractWrite({
     enabled: isConnected && allowanceContractRead.isSuccess,
     address: nfteContractAddresses[chain?.id || CHAIN_ID],
     abi: ABI,
@@ -41,30 +39,64 @@ export default function AllowanceButton() {
     ],
   });
 
-  const contractWrite = useContractWrite(config);
+  const { config: depositConfig } = usePrepareContractWrite({
+    enabled: isConnected && allowanceContractRead.isSuccess,
+    address: stakingContractAddresses[chain?.id || CHAIN_ID],
+    abi: ABI,
+    functionName: "depositNfte",
+    args: [
+      // Provide necessary arguments for the depositNfte function
+    ],
+  });
 
-  const waitForTransaction = useWaitForTransaction({
-    hash: contractWrite.data?.hash,
+  const approveContractWrite = useContractWrite(approveConfig);
+  const depositContractWrite = useContractWrite(depositConfig);
+
+  const waitForApproveTransaction = useWaitForTransaction({
+    hash: approveContractWrite.data?.hash,
     confirmations: 2,
     onSuccess() {
-      allowanceContractRead.refetch();
-      contractWrite.reset();
+      approveContractWrite.reset();
     },
   });
 
+  const waitForDepositTransaction = useWaitForTransaction({
+    hash: depositContractWrite.data?.hash,
+    confirmations: 2,
+    onSuccess() {
+      depositContractWrite.reset();
+    },
+  });
+
+  const handleApproveClick = () => {
+    approveContractWrite.write?.();
+  };
+
+  const handleDepositClick = () => {
+    depositContractWrite.write?.();
+  };
+
   return (
-    <button
-      className="ml-2 border px-3 py-2 hover:border-gray-500 disabled:text-gray-400 disabled:hover:cursor-not-allowed dark:border-slate-500 dark:bg-slate-800  dark:hover:border-slate-300"
-      disabled={
-        contractWrite.isLoading ||
-        waitForTransaction.isFetching ||
-        waitForTransaction.isLoading
-      }
-      onClick={() => {
-        contractWrite.write?.();
-      }}
-    >
-      Approve NFTE Staking
-    </button>
+    <div>
+      {!allowanceContractRead.isSuccess && (
+        <button
+          className="ml-2 border px-3 py-2 hover:border-gray-500 disabled:text-gray-400 disabled:hover:cursor-not-allowed dark:border-slate-500 dark:bg-slate-800  dark:hover:border-slate-300"
+          disabled={approveContractWrite.isLoading || waitForApproveTransaction.isFetching || waitForApproveTransaction.isLoading}
+          onClick={handleApproveClick}
+        >
+          Approve NFTE Staking Allowance
+        </button>
+      )}
+
+      {allowanceContractRead.isSuccess && (
+        <button
+          className="ml-2 border px-3 py-2 hover:border-gray-500 disabled:text-gray-400 disabled:hover:cursor-not-allowed dark:border-slate-500 dark:bg-slate-800  dark:hover:border-slate-300"
+          disabled={depositContractWrite.isLoading || waitForDepositTransaction.isFetching || waitForDepositTransaction.isLoading}
+          onClick={handleDepositClick}
+        >
+          Stake NFTE
+        </button>
+      )}
+    </div>
   );
 }
