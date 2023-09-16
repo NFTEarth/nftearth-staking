de5d58bbbb83304be99e5d16b830cee101b1f440
Weston Nelson <dogooddowell@protonmail.com>
2023-09-16 15:52:40 -0500

    chore: update staking contract address
    add xNFTE staking contract address
    
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 import {Map} from "./types/map";
 
-export const NFTE_STAKING_CONTRACT_ADDRESS = '0xb37cd5fF087116B6Af620C69DeC2a03Ca5e5CaDe';
+export const NFTE_STAKING_CONTRACT_ADDRESS = '0xe57bd15448c3b2d1dbad598775dd2f36f93ebf90';
 export const NFTE_TOKEN_ADDRESS = '0x51b902f19a56f0c8e409a34a215ad2673edf3284';
 export const WETH_TOKEN_ADDRESS = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1';
 export const CHAIN_ID = 42161
